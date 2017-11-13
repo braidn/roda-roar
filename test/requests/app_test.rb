e61279e1763b0f546881be37bdec6cbf975c6642
Braden <braden.douglass@gmail.com>
2017-11-12 21:33:02 -0500

    App Test Driven Into Requests.
    Requests specs will drive any high level, non controller based logic.
    These tests routes will generally be inconsequential and not do too
    much.

---
+++ test/requests/app_test.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require 'rack/test'
+require_relative '../../app'
+
+describe App do
+  include Rack::Test::Methods
+
+  def app
+    App.freeze.app
+  end
+
+  it 'instantiates itself successfully' do
+    app.wont_be_nil
+  end
+
+  describe 'root (/) path' do
+    before do
+      get '/'
+    end
+
+    it 'responds successfully' do
+      last_response.ok?
+    end
+  end
+end
